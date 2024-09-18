76874ce9cf9207d49e8ab1755f70ed6edbf48819
Abhijitraut9921 <abhijitraut74@gmail.com>
2024-09-18 20:15:53 +0530

    data_addeing and reading from mongodb is done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Abhi",
+    author_email="abhijitraut0105@gmail.com",
+    packages=find_packages()
+
+)
